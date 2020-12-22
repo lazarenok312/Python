764577a3ba36ce1a1e473ae0621a304f8ba77b54
Лазарев Дмитрий <69201114+lazarenok312@users.noreply.github.com>
2020-12-22 19:16:56 +0600

    Create stack.py

---
+++ home4/stack.py
@@ -0,0 +1,16 @@
+def create_stack():
+i = []
+
+def push(element, stack):
+	i.append(element)
+
+def remove(stack):
+	i.dell(stack)
+
+def is_empty(stack):
+	if not i:
+   return True
+
+def calc(stack):
+
+
