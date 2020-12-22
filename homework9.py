0e0896b5953a0ccc324cd0067976033db3d1e9dd
Лазарев Дмитрий <69201114+lazarenok312@users.noreply.github.com>
2020-12-22 19:17:33 +0600

    Create homework9.py

---
+++ homework9.py
@@ -0,0 +1,26 @@
+import csv
+
+data = ["id,username,email,ip_address", 
+        "1,root,root@example.com,192.168.0.1", 
+        "2,admin,admin@example.com,192.168.0.2", 
+        "3,test_user,test_user@example.com,192.168.0.3", 
+        "4,second_user,second_user@example.com,192.168.0.4"]
+
+path = "C:\\Users\\lazar\\Desktop\\Homework\\data.csv"
+
+
+def csv_write(data, path):
+    with open(path, 'w') as csv_file:
+       csv_writer = csv.writer(csv_file)
+       for line in data:
+            csv_writer.writerow(line.split(","))
+
+csv_write(data, path)
+
+def csv_read(path):
+    with open(path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print("       ".join(row))
+
+csv_read(path)
