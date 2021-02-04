7e114e91fc42606bfdc3822289f0ee6249d81fee
Лазарев Дмитрий <69201114+lazarenok312@users.noreply.github.com>
2021-02-04 14:46:39 +0600

    Create admin.py

---
+++ FlankyShop/shop/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from .models import Category, Subcategory, Product
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'subcategory', 'price', 'description']}),
+    ]
+
+    list_display = ('name', 'price', 'available', 'created', 'updated')
+    list_filter = ['subcategory', 'price']
+    list_editable = ['price', 'available']
+    search_fields = ['subcategory', 'name', 'description']
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'category', 'body']}),
+    ]
+
+    list_display = ('name', 'category', 'body')
+    list_filter = ['category']
+    search_fields = ['category', 'name', 'body']
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'body']}),
+    ]
+
+    list_display = ('name', 'body')
+    search_fields = ['name', 'body']
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
+admin.site.register(Product, ProductAdmin)
